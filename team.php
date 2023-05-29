a071faf9ba25e5b173b669a485c8ad2d2f3e4149
kushvijay234 <kushwahavijay234@gmail.com>
2023-05-29 19:31:31 +0530

    final commit

--- team.php
+++ team.php
@@ -2,16 +2,16 @@
 <?php include 'Header.php'; ?>    
 <div class="container">   
         <div class="row">
-            <div class="col-md-3">
-                <img src="assets/vijay.jpg" id="gallery" alt="Image 1" class="img-fluid">
+            <div class="col-md-3 gallery">
+                <img src="assets/vijay.jpg" id="gallery"  alt="Image 1" class="img-fluid">
                 <h4 style="text-align:center">Team Leader</h4>
             </div>
             <div class="col-md-3">
-                <img src="assets/krishna.jpg" id="gallery" alt="Image 2" class="img-fluid">
+                <img src="assets/krishna.jpg" id="gallery"  alt="Image 2" class="img-fluid">
                 <h4 style="text-align:center">Member</h4>
             </div>
             <div class="col-md-3">
-                <img src="assets/abhay.jpg" id="gallery" alt="Image 3" class="img-fluid">
+                <img src="assets/abhay.jpg" id="gallery"  alt="Image 3" class="img-fluid">
                 <h4 style="text-align:center">Member</h4>
             </div>
             <div class="col-md-3">
