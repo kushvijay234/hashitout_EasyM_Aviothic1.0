17f05f39865544cb5083b5e303f25e143afd2b61
kushvijay234 <kushwahavijay234@gmail.com>
2023-05-29 17:24:41 +0530

    f1

--- home.php
+++ home.php
@@ -5,25 +5,26 @@
       <div class="container hero">
         <h2>Welcome to Easy M</h2>
         <p class="lead">Portal For local </p>
-        <a href="#" class="btn btn-primary">Shop Now</a>
+        <a href="http://localhost/hashitout_EasyM_Aviothic1.0/business.php" class="btn btn-primary">Shop Now</a>
       </div>
     </section>
 
     <!-- features Section -->
-    <div class="container features">
+    <div class="container features">   
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <img src="assets/banner-sec-1.jpg" alt="Image 1" class="img-fluid">
+                <h4 style="text-align:center">Easy to Use</h4>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <img src="assets/banner-sec-2.jpg" alt="Image 2" class="img-fluid">
+                <h4 style="text-align:center">User Friendly</h4>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <img src="assets/banner-sec-3.jpg" alt="Image 3" class="img-fluid">
+                <h4 style="text-align:center">Online Shopping</h4>
             </div>
-            <div class="col-md-3">
-                <img src="assets/banner-sec-4.webp" alt="Image 4" class="img-fluid">
-            </div>
+    
         </div>
     </div>
 <?php include 'footer.php'; ?>
--- team.php
+++ team.php
@@ -1,5 +1,25 @@
 
 <?php include 'Header.php'; ?>    
-Team Is Work!
+<div class="container">   
+        <div class="row">
+            <div class="col-md-3">
+                <img src="assets/vijay.jpg" id="gallery" alt="Image 1" class="img-fluid">
+                <h4 style="text-align:center">Team Leader</h4>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/krishna.jpg" id="gallery" alt="Image 2" class="img-fluid">
+                <h4 style="text-align:center">Member</h4>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/abhay.jpg" id="gallery" alt="Image 3" class="img-fluid">
+                <h4 style="text-align:center">Member</h4>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/Halwai.jpg" id="gallery" alt="Image 3" class="img-fluid">
+                <h4 style="text-align:center">Member</h4>
+            </div>
+    
+        </div>
+    </div>
 <?php include 'footer.php'; ?>
 
